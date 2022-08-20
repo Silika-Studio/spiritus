f113544db044d13b32c9c759b4e8b8639c771951
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-08-20 12:23:59 -0700

    include font files

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -11,9 +11,12 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" /> */}
+
+                    <link rel="preload" href="/fonts/Poppins/Poppins-Medium.ttf" as="font" crossOrigin="" />
+                    <link rel="preload" href="/fonts/Poppins/Poppins-Bold.ttf" as="font" crossOrigin="" />
                 </Head>
                 <body style={{ fontFamily: "Poppins, sans-serif" }}>
                     <Main />

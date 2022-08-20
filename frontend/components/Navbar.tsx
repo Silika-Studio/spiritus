77af09f94b9194aaeba12974d744b86dde44fd88
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-08-19 21:56:14 -0700

    FIXED

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { HStack, Stack, Text } from "@chakra-ui/react";
 export const Navbar: React.FC = () => {
 
     return (
-        <Stack zIndex="1000" position="absolute" top="0px" left="0px" h="200px" w="100vw" justifyContent="start">
+        <Stack zIndex="1000" position="fixed" top="0px" left="0px" h="200px" w="100vw" justifyContent="start">
             <HStack h="144px" w="100%" blur="8px" paddingX="32px">
                 <Text fontFamily="Poppins" color="white" fontSize="36px" fontWeight="bold">Spiritus</Text>
 

446e5e168f97c1a5ed6b220d5ee05df504aa7462
Ilan 🌝 <ilanbrianrosen@gmail.com>
2022-08-21 04:07:58 -0700

    cleanup

--- server/services/utils.ts
+++ server/services/utils.ts
@@ -1,60 +1,60 @@
 import { ethers } from "ethers";
 import { Interface } from "ethers/lib/utils";
 
-import { NFTStorage, File } from "nft.storage";
 import fs from "fs";
 import mime from "mime";
+import { File, NFTStorage } from "nft.storage";
 import path from "path";
 import retry = require("async-retry");
 // The NFT.Storage API token, passed to `NFTStorage` function as a `token`
 const nftStorageApiKey = process.env.NFT_STORAGE_API_KEY;
 
 export const NOUNS_CONTRACT_ADDRESS =
-  "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03";
+    "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03";
 export const OPENSEA_IPFS_GATEWAY = "https://opensea.mypinata.cloud";
 export type Network = "mainnet" | "polygon-mumbai";
 
 export const ipfsLocationToHttpsGateway = (ipfsLoc: string) =>
-  ipfsLoc.includes("ipfs://")
-    ? `${OPENSEA_IPFS_GATEWAY}/${ipfsLoc.replace("ipfs://", "ipfs/")}`
-    : ipfsLoc;
+    ipfsLoc.includes("ipfs://")
+        ? `${OPENSEA_IPFS_GATEWAY}/${ipfsLoc.replace("ipfs://", "ipfs/")}`
+        : ipfsLoc;
 
 const providerInstanceMainnet = new ethers.providers.AlchemyProvider(
-  "mainnet",
-  process.env.ALCHEMY_API_KEY
+    "mainnet",
+    process.env.ALCHEMY_API_KEY
 );
 const providerInstancePolygonMumbai = new ethers.providers.AlchemyProvider(
-  "maticmum",
-  process.env.ALCHEMY_API_KEY
+    "maticmum",
+    process.env.ALCHEMY_API_KEY
 );
 
 export const getProvider = (network: Network) => {
-  switch (network) {
-    case "mainnet":
-      return providerInstanceMainnet;
-    case "polygon-mumbai":
-      return providerInstancePolygonMumbai;
-  }
+    switch (network) {
+        case "mainnet":
+            return providerInstanceMainnet;
+        case "polygon-mumbai":
+            return providerInstancePolygonMumbai;
+    }
 };
 
 export const getABI = (contractAddress: string) => {
-  return new Interface(
-    fs.readFileSync(`./abis/${contractAddress}.json`, { encoding: "utf-8" })
-  );
-  // return JSON.stringify();
+    return new Interface(
+        fs.readFileSync(`./abis/${contractAddress}.json`, { encoding: "utf-8" })
+    );
+    // return JSON.stringify();
 };
 
 const jsonEncodedPrepend = "application/json;base64,";
 
 export const decodeBase64TokenURI = (tokenURI: string) => {
-  return JSON.parse(
-    Buffer.from(
-      tokenURI.substring(
-        tokenURI.indexOf("application/json;base64,") + jsonEncodedPrepend.length
-      ),
-      "base64"
-    ).toString("ascii")
-  );
+    return JSON.parse(
+        Buffer.from(
+            tokenURI.substring(
+                tokenURI.indexOf("application/json;base64,") + jsonEncodedPrepend.length
+            ),
+            "base64"
+        ).toString("ascii")
+    );
 };
 
 import axios from "axios";
@@ -63,94 +63,94 @@ import axios from "axios";
 import https from "https";
 import sharp from "sharp";
 import {
-  LayerMap,
-  LoadTraitType,
-  Trait,
-  UploadCollectionTraits,
+    LayerMap,
+    LoadTraitType,
+    Trait,
+    UploadCollectionTraits
 } from "../types";
 
 const ipfsGatewayUrl = "https://ipfs.moralis.io:2053/ipfs/";
 
 export const loadAllLayers = async (layersURI: string) => {
-  // const layers: LoadCollectionTraits = {
-  //     // layerHash: 'QmZ6mcScDMKiYt49fddbMzFwmfmc6os2a7QsbeJ7ocZP2M'
-  //     layerHash: 'QmaJejnUuxHgT7dyUTKusDGnTfgnwBnQBRk4nReUrNAT4U'
-  //     , collectionName: 'nouns', traits: {}
-  // };
+    // const layers: LoadCollectionTraits = {
+    //     // layerHash: 'QmZ6mcScDMKiYt49fddbMzFwmfmc6os2a7QsbeJ7ocZP2M'
+    //     layerHash: 'QmaJejnUuxHgT7dyUTKusDGnTfgnwBnQBRk4nReUrNAT4U'
+    //     , collectionName: 'nouns', traits: {}
+    // };
 
-  const layers: Record<string, Record<string, LoadTraitType>> = {};
-  const r = await axios.get<UploadCollectionTraits>(layersURI);
-  const httpsAgent = new https.Agent({ keepAlive: true });
-  const promises: Promise<void>[] = [];
-  Object.entries(r.data.traits).forEach(([traitType, traits]) => {
-    traits.forEach((trait, i) => {
-      const get = async () => {
-        await new Promise((resolve) => {
-          setTimeout(resolve, i * 40);
+    const layers: Record<string, Record<string, LoadTraitType>> = {};
+    const r = await axios.get<UploadCollectionTraits>(layersURI);
+    const httpsAgent = new https.Agent({ keepAlive: true });
+    const promises: Promise<void>[] = [];
+    Object.entries(r.data.traits).forEach(([traitType, traits]) => {
+        traits.forEach((trait, i) => {
+            const get = async () => {
+                await new Promise((resolve) => {
+                    setTimeout(resolve, i * 40);
+                });
+                const buffer = Buffer.from(
+                    await sharp(
+                        (
+                            await retry(
+                                async (bail) => {
+                                    const res = await axios.get(
+                                        `${ipfsGatewayUrl}${r.data.rootHash}/${trait.url}`,
+                                        { responseType: "arraybuffer", httpsAgent }
+                                    );
+                                    if (res == null) {
+                                        throw new Error("not found, try again");
+                                    }
+
+                                    return res;
+                                },
+                                {
+                                    retries: 5,
+                                }
+                            )
+                        ).data
+                    ).toBuffer()
+                );
+
+                if (!layers[traitType]) layers[traitType] = {};
+                layers[traitType][trait.value] = { data: buffer, ...trait };
+            };
+            promises.push(get());
         });
-        const buffer = Buffer.from(
-          await sharp(
-            (
-              await retry(
-                async (bail) => {
-                  const res = await axios.get(
-                    `${ipfsGatewayUrl}${r.data.rootHash}/${trait.url}`,
-                    { responseType: "arraybuffer", httpsAgent }
-                  );
-                  if (res == null) {
-                    throw new Error("not found, try again");
-                  }
-
-                  return res;
-                },
-                {
-                  retries: 5,
-                }
-              )
-            ).data
-          ).toBuffer()
-        );
-
-        if (!layers[traitType]) layers[traitType] = {};
-        layers[traitType][trait.value] = { data: buffer, ...trait };
-      };
-      promises.push(get());
     });
-  });
 
-  await Promise.all(promises);
+    await Promise.all(promises);
 
-  console.log(
-    `Successfully downloaded ${Object.keys(layers).length} trait types!`
-  );
+    console.log(
+        `Successfully downloaded ${Object.keys(layers).length} trait types!`
+    );
 
-  console.log(layers);
-  return layers;
+    console.log(layers);
+    return layers;
 };
 
 export const generateImage = async (traits: Trait[], layerMap: LayerMap) => {
-  const now = Date.now();
-  await sharp(layerMap[traits[0].traitType][traits[0].value].data)
-    .composite(
-      traits
-        .slice(1)
-        .map((t) => ({ input: layerMap[t.traitType][t.value].data }))
-    )
-    .toFile(`./bin/${now}.png`);
+    const now = Date.now();
+    await sharp(layerMap[traits[0].traitType][traits[0].value].data)
+        .composite(
+            traits
+                .slice(1)
+                .map((t) => ({ input: layerMap[t.traitType][t.value].data }))
+        )
+        .toFile(`./bin/${now}.png`);
 
-  const img = fs.readFileSync(`./bin/${now}.png`, { encoding: "base64" });
+    const img = fs.readFileSync(`./bin/${now}.png`, { encoding: "base64" });
 
-  async function fileFromPath(filePath: string) {
-    const content = await fs.promises.readFile(filePath);
-    const type = mime.getType(filePath);
-    return new File([content], path.basename(filePath), { type });
-  }
-  const image = await fileFromPath(img);
-  // Upload to IPFS using NFT Storage
-  const storage = new NFTStorage({ token: nftStorageApiKey });
-  const imageCid = await storage.storeBlob(image);
+    async function fileFromPath(filePath: string) {
+        const content = await fs.promises.readFile(filePath);
+        const type = mime.getType(filePath);
+        return new File([content], path.basename(filePath), { type: type ?? undefined });
+    }
+    const image = await fileFromPath(img);
+    // Upload to IPFS using NFT Storage
+    const storage = new NFTStorage({ token: nftStorageApiKey! });
+    const imageCid = await storage.storeBlob(image);
 
-  const uri = `https://${imageCid}.ipfs.nftstorage.link/`;
+    const uri = `https://${imageCid}.ipfs.nftstorage.link/`;
 
-  return uri;
+    return uri;
 };
